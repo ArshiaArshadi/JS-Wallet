8d7c47b72ec7de5417928fe856d53ce3552df9fa
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-12-14 20:23:49 +0330

    add : creat & add base file
    .

---
+++ js/app.js
@@ -0,0 +1,66 @@
+let totalIncome = prompt('Please Add Your Weekly Income');
+ 
+
+if(totalIncome !== ''){
+    document.querySelector('#total').textContent = totalIncome;
+    document.querySelector('#left').textContent = totalIncome
+}else{
+    while(totalIncome == ''){
+        totalIncome = prompt('Please Add Your Weekly Income');
+        if(totalIncome !== ''){
+            document.querySelector('#total').textContent = totalIncome;
+            document.querySelector('#left').textContent = totalIncome
+        }
+    }
+}
+
+const btn = document.querySelector('.btn-primary');
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const expense = document.querySelector('#expense')
+    const amount = document.querySelector('#amount')
+    const ul = document.querySelector('.list-group');
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const span2 = document.createElement('span');
+    span.textContent = expense.value;
+    span2.textContent = amount.value;
+    li.appendChild(span);
+    li.appendChild(span2);
+    ul.appendChild(li);
+    expense.value = '';
+    amount.value = '';
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
